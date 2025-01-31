8ace94bee8b03a38cdfad34e0a3cc777f9efaa34
Stephen McElhose <smcelhose.aws@gmail.com>
2025-01-30 18:08:44 -0700

    fix header position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import UserList from "../components/users/user-list";
 export default function Home() {
   return (
     <div>
-      <div className="container justify-center items-center mx-auto">
+      <div className="container justify-center items-center mx-auto max-w-screen-md">
         <h1 className="text-6xl font-bold my-16">Find a Friend</h1>
         <Suspense
           fallback={
